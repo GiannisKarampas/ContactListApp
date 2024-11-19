d2ee34f7dbd8ad5bf16a419fa846e2332d990fdc
Giannis <g.karampas91@gmail.com>
2024-11-19 16:55:40 +0200

    New test case

--- main/utils/common/SetupConfig.utils.ts
+++ main/utils/common/SetupConfig.utils.ts
@@ -4,13 +4,12 @@ import * as path from "path";
 export class SetupConfig {
     public static setPathForConfigFile() {
         const dirname = __dirname.replace(path.join("main", "utils", "common"), "");
+
         if (!process.env.CONFIG) {
             process.env.CONFIG = "development";
         }
 
-        const configPath = dirname + `resources/config/.config.${process.env.CONFIG}`;
-        console.log(`Loading config from: ${configPath}`);
-
+        const configPath = path.join(dirname, "resources", "config", `.config.${process.env.CONFIG}`);
         dotenv.config({
             path: configPath,
             override: false,
--- test/gui/Invalid_dob.test.ts
+++ test/gui/Invalid_dob.test.ts
@@ -1,13 +1,22 @@
 import {test} from "@fixtures/Fixtures"
 import {expect} from "@playwright/test";
 import {CreateRandomUser} from "@pages/CreateNewUser.page";
+import {UserObj} from "@interfaces/UserObject";
+import {APIUtils} from "@utils/common/Api.utils";
+
+let user: UserObj;
+
+test.beforeAll(async () => {
+
+    user = CreateRandomUser.createRandomUser();
+    await APIUtils.addNewUserAndGetToken(user);
+})
 
 test("Add a contact with invalid date of birth and validate the error message.", async ({page, login, newContact}) => {
     const errorMessage = page.locator("#error");
-    const user = CreateRandomUser.createRandomUser();
 
     await page.goto("/")
-    await login.loginUser(process.env.USERNAME, process.env.PASSWORD);
+    await login.loginUser(user.email, user.password);
 
     await newContact.createNewContactWithWrongDob(user)
 
